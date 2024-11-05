521ccd51de5b695a5df84188aada0854d1cb445c
anonymousc <dodmin15@gmail.com>
2024-11-05 01:21:40 +0100

    PUSH : adding some edits to unset also pulling

--- execution/my_unset.c
+++ execution/my_unset.c
@@ -1,6 +1,6 @@
 #include "builtins.h"
 
-void my_unset(t_exec **exec, char *s) 
+void unset_var(t_exec **exec, char *s) 
 {
     t_env *curr = (*exec)->env;
     t_env *to_remove = NULL;
@@ -28,25 +28,28 @@ void my_unset(t_exec **exec, char *s)
     }
 }
 
+int my_unset(t_exec **exec)
+{
+	int i = 1;
+	while ((*exec)->av[i])
+	{
+		unset_var(exec, (*exec)->av[i]);
+		i++;
+	}
+	return 0;
+}
 
-// int main(int ac ,char **av , char **envp)
-// {
-// 	t_exec *exec = malloc(sizeof(t_exec));
-// 	exec->env_orginal = envp;
-// 	t_env *env = make_env(exec);
-// 	exec->env = env;
-// 	exec->av = av;
-// 	exec->ac = ac;
-// 	int i = 0;
-// 	while(ac > ++i)
-// 	{
-// 		my_unset(&exec , av[i]);
-// 		i++;
-// 	}
-// 	t_env *current = exec->env;
-// 	while (current)
-// 	{
-// 		printf ("%s=%s\n", current->variable, current->value);
-// 		current = current->next;
-// 	}
-// }
+int main(int ac ,char **av , char **envp)
+{
+	t_exec *exec = malloc(sizeof(t_exec));
+	exec->env_orginal = envp;
+	t_env *env = make_env(exec);
+	exec->env = env;
+	exec->av = av;
+	exec->ac = ac;
+	int i = 0;
+	my_unset(&exec);
+	t_env *current = exec->env;
+	my_env(&current);
+	free_env(&current);
+}
